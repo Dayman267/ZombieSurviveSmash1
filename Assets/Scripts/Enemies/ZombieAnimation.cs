00f2cface69b2ad4e1298973473688dba0207c85
Dayman267 <epicdayman@gmail.com>
2022-11-07 18:16:18 +0200

    added zombie death, player death, zombie bite, edited rocks prefab

--- Assets/Scripts/Enemies/ZombieAnimation.cs
+++ Assets/Scripts/Enemies/ZombieAnimation.cs
@@ -13,17 +13,36 @@ public class ZombieAnimation : MonoBehaviour
     private Transform player;
     [SerializeField, Range(0, 5)] private float biteDistanse = 1.5f;
 
+    private ZombieHealth health;
+    private PlayerHealth playerHealth;
+
+    [SerializeField] private float biteDamage;
+
     private void Start()
     {
         endVector = transform.position;
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        health = GetComponent<ZombieHealth>();
+        playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     private void FixedUpdate()
     {
         startVector = transform.position;
-        if (startVector != endVector)
+        if(health.GetHealth() <= 0)
+        {
+            ChangeAnimation("zombie_death");
+            Destroy(gameObject.GetComponent<ZombieMovement>());
+            Destroy(gameObject.GetComponent<CapsuleCollider2D>());
+            Destroy(gameObject.GetComponent<Animator>(), 0.8f);
+            Destroy(gameObject.GetComponent<CircleCollider2D>());
+            Destroy(gameObject.GetComponent<PositionRendererSorter>());
+            Destroy(gameObject.GetComponent<Rigidbody2D>());
+            Destroy(gameObject.GetComponent<ZombieHealth>());
+            Destroy(gameObject.GetComponent<ZombieAnimation>());
+        }
+        else if (startVector != endVector)
         {
             endVector = startVector;
             ChangeAnimation("zombie_idle");
@@ -31,6 +50,7 @@ public class ZombieAnimation : MonoBehaviour
         else if (Vector2.Distance(transform.position, player.position) <= biteDistanse)
         {
             ChangeAnimation("zombie_bite");
+            playerHealth.TakeDamage(biteDamage);
         }
         else
         {
@@ -38,6 +58,16 @@ public class ZombieAnimation : MonoBehaviour
         }
 
     }
+
+    /*private IEnumerator BiteCoroutine()
+    {
+        if(Vector2.Distance(transform.position, player.position) <= biteDistanse)
+        {
+            playerHealth.TakeDamage(biteDamage);
+            yield return new WaitForSeconds(1);
+        }
+        yield return new WaitForFixedUpdate();
+    }*/
 
     private void ChangeAnimation(string animation)
     {
--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private GameObject hitEffect;
+    [SerializeField] private float damage;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 0.517f);
-        Destroy(gameObject);
+        if (!collision.gameObject.tag.Equals("Player"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.517f);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.GetComponent<ZombieHealth>())
+        {
+            collision.gameObject.GetComponent<ZombieHealth>().TakeDamage(damage);
+        }
     }
 }
