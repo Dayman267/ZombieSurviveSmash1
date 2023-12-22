c7f8fb263fecd4248cb050fc9a4975ae6133c617
VetalyaRabotyaga <vitalii.lutsenko@nure.ua>
2023-12-23 01:07:59 +0200

    Temporary Lobby
    lobby for localhost

--- Assets/AssetsDD/Scripts/Menu/Connection.cs
+++ Assets/AssetsDD/Scripts/Menu/Connection.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class Connection : MonoBehaviour
 {
+    public InputField inputField;
     public NetworkManager networkManager;
  
     private void Start()
     {
-        if (!Application.isBatchMode)
+        /*if (!Application.isBatchMode)
         {
             networkManager.StartClient();
-        }
+        }*/
     }
 
 
     public void JoinClient()
     {
-        networkManager.networkAddress = "localhost";
+        if (inputField.text == "" || inputField.text == "Enter IP address")
+        {
+            networkManager.networkAddress = "localhost";
+        }
+        else
+        {
+            networkManager.networkAddress = inputField.text;
+        }
+
         networkManager.StartClient();
     }
 }
